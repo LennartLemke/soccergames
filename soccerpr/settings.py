924a913db14230a332545ca0a2c22529830419fb
Fleshless <lennart-lemke@hotmail.com>
2022-03-10 22:22:53 +0100

    ready for Heroku

--- soccerpr/settings.py
+++ soccerpr/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,3 +122,6 @@ STATICFILES_DIR = [os.path.join (BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Heroku settings for Django
+django_heroku.settings(locals())
